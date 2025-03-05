7a15aa54423494ff1d712c08803bea6377b685f4
Bonk03 <135335034+Bonk03@users.noreply.github.com>
2025-03-05 13:35:08 +0100

    Update tri_gate_sim.py with visualization of hexapod

--- moving_codes/tri_gate_sim.py
+++ moving_codes/tri_gate_sim.py
@@ -1,33 +1,52 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+from matplotlib.animation import FuncAnimation
 
-# dlugosci segmentow nog
-L1 = 1
+matplotlib.use('TkAgg')
+
+
+def polozenie_przegub_1(l1, alfa1, przyczep):
+    return np.array([l1 * np.cos(alfa1) + przyczep[0], l1 * np.sin(alfa1) + przyczep[1], przyczep[2]])
+
+
+def polozenie_przegub_2(l1, l2, alfa1, alfa2, przyczep):
+    return polozenie_przegub_1(l1, alfa1, przyczep) + np.array(
+        [l2 * np.cos(alfa1) * np.cos(alfa2), l2 * np.sin(alfa1) * np.cos(alfa2), l2 * np.sin(alfa2)])
+
+
+# Długosci segmentow nog
+L1 = 3
 L2 = 5
 L3 = 6
 
-#położenie punktu spoczynku stopy od przyczepu nogi w układzie wspolrzednych danej nogi a nie srodka pajaka!!!
+# Położenie punktu spoczynku od przyczepu nogi wyznaczone na bazie katow przgubow podczas spoczynku
+# WAZNE !!! jest to polozenie stopy w ukladzie punktu zaczepienia stopy a nie ukladu XYZ
+# w ktorym X1 to prostopadła prosta do boku platformy do ktorej noga jest zaczepiona i rosnie w kierunku od hexapoda
+# Y1 to os pokrywajaca sie z bokiem platformy do ktorego jest przyczepiona noga i rosnie w kierunku przodu hexapoda
+# Z1 pokrywa sie z osia Z ukladu XYZ
+
+# zalozone katy spoczynkowe przegubow
 alfa_1 = 0
-alfa_2 = np.pi / 180 * 30
-alfa_3 = np.pi / 180 * 90
+alfa_2 = np.radians(30)
+alfa_3 = np.radians(90)
 
-P1_X = L1 * np.cos(alfa_1)
-P1_Y = L1 * np.sin(alfa_1)
-P1_Z = 0
+P1 = np.array([L1 * np.cos(alfa_1), L1 * np.sin(alfa_1), 0])
+P2 = P1 + np.array([
+    L2 * np.cos(alfa_1) * np.cos(alfa_2),
+    L2 * np.sin(alfa_1) * np.cos(alfa_2),
+    L2 * np.sin(alfa_2)
+])
 
-P2_X = P1_X + L2 * np.cos(alfa_1) * np.cos(alfa_2)
-P2_Y = P1_Y + L2 * np.sin(alfa_1) * np.cos(alfa_2)
-P2_Z = L2 * np.sin(alfa_2)
+stopa_spoczynkowa = P2 + np.array([
+    L3 * np.cos(alfa_1) * np.sin(alfa_3 - (np.pi / 2 - alfa_2)),
+    L3 * np.sin(alfa_1) * np.sin(alfa_3 - (np.pi / 2 - alfa_2)),
+    -L3 * np.cos(alfa_3 - (np.pi / 2 - alfa_2))
+])
 
-x_spocz = P2_X + L3 * np.cos(alfa_1) * np.sin(alfa_3 - (np.pi / 2 - alfa_2))
-y_spocz = P2_Y + L3 * np.sin(alfa_1) * np.sin(alfa_3 - (np.pi / 2 - alfa_2))
-z_spocz = P2_Z - L3 * np.cos(alfa_3 - (np.pi / 2 - alfa_2))
+wysokosc_start = -stopa_spoczynkowa[2]
 
-print(x_spocz, y_spocz, z_spocz)
-
-wysokosc_start = -z_spocz
-z_spocz = 0
-
-# definicja punktow na platformie pajaka
+# Punkty tulowia
 tulow = np.array([
     [2, 6, wysokosc_start],
     [4, 2, wysokosc_start],
@@ -39,23 +58,90 @@ tulow = np.array([
     [-2, 6, wysokosc_start],
 ])
 
-#pozycje w przestrzeni przyczepow nog do tulowia
-
-przyczepy_nog_do_tulowia= np.array([
-    #noga 1
-    [(tulow[0][0] + tulow[1][0]) / 2, (tulow[0][1] + tulow[1][1]) / 2, wysokosc_start],
-    #noga 2
-    [(tulow[1][0] + tulow[2][0]) / 2, (tulow[1][1] + tulow[2][1]) / 2, wysokosc_start],
-    #noga 3
-    [(tulow[2][0] + tulow[3][0]) / 2, (tulow[2][1] + tulow[3][1]) / 2, wysokosc_start],
-    #noga 4
-    [(tulow[4][0] + tulow[5][0]) / 2, (tulow[4][1] + tulow[5][1]) / 2, wysokosc_start],
-    #noga 5
-    [(tulow[5][0] + tulow[6][0]) / 2, (tulow[5][1] + tulow[6][1]) / 2, wysokosc_start],
-    #noga 6
-    [(tulow[6][0] + tulow[7][0]) / 2, (tulow[6][1] + tulow[7][1]) / 2, wysokosc_start],
+# Pozycje spoczynkowe nog
+przyczepy_nog_do_tulowia = np.array([
+    (tulow[i] + tulow[(i + 1) % 8]) / 2 for i in [0, 1, 2, 4, 5, 6]
 ])
 
-#todo polozenie przegubow i stop podczas pozycji spoczynkowej pajaka
+# Nachylenia bokow platformy w widoku od gory na os XY
+nachylenia_bokow_platformy_pajaka = np.array([
+    np.atan2(tulow[i + 1][1] - tulow[i][1], tulow[i + 1][0] - tulow[i][0]) + np.pi / 2
+    for i in [0, 1, 2, 4, 5, 6]
+])
 
-# cos z tym popgrzebac --> np.tan((np.abs(tulow[0][0] - tulow[1][0])) / np.abs((tulow[0][1] - tulow[1][1]))))
+# Polozenie spoczynkowe stop
+polozenie_spoczynkowe_stop = np.array([
+    przyczepy_nog_do_tulowia[i] + np.array([
+        stopa_spoczynkowa[0] * np.cos(nachylenia_bokow_platformy_pajaka[i]) -
+        stopa_spoczynkowa[1] * np.sin(nachylenia_bokow_platformy_pajaka[i]),
+
+        stopa_spoczynkowa[0] * np.sin(nachylenia_bokow_platformy_pajaka[i]) +
+        stopa_spoczynkowa[1] * np.cos(nachylenia_bokow_platformy_pajaka[i]),
+
+        stopa_spoczynkowa[2]
+    ]) for i in range(6)
+])
+
+# Obliczanie pozycji nog (przyczep, przeguby, stopa)
+polozenie_nog = [
+    [
+        przyczepy_nog_do_tulowia[i],
+        polozenie_przegub_1(L1, alfa_1 + nachylenia_bokow_platformy_pajaka[i], przyczepy_nog_do_tulowia[i]),
+        polozenie_przegub_2(L1, L2, alfa_1 + nachylenia_bokow_platformy_pajaka[i], alfa_2, przyczepy_nog_do_tulowia[i]),
+        polozenie_spoczynkowe_stop[i]
+    ] for i in range(6)
+]
+
+# Wizualizacja w 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+fig.canvas.draw_idle()
+
+# Rysowanie tulowia
+ax.plot(tulow[:, 0], tulow[:, 1], tulow[:, 2], 'bo-')
+ax.plot([tulow[0, 0], tulow[7, 0]], [tulow[0, 1], tulow[7, 1]], [tulow[0, 2], tulow[7, 2]], c='b', lw=2)
+
+
+# Rysowanie nog
+for i in range(6):
+    noga = polozenie_nog[i]
+
+    # Rysowanie linii nog
+    ax.plot([noga[j][0] for j in range(4)],
+            [noga[j][1] for j in range(4)],
+            [noga[j][2] for j in range(4)], 'ro-')
+
+    # Rysowanie punktow przegubow i stop
+    colors = ['g', 'y', 'r', 'b']  # Kolory: przyczep, przegub1, przegub2, stopa
+    for j in range(4):
+        ax.scatter(noga[j][0], noga[j][1], noga[j][2], s=50, c=colors[j])
+
+# Oznaczenia osi
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title("Model hexapoda 3D")
+ax.legend()
+
+# Wyskalowanie wykresu
+x_vals = [noga[i][0] for noga in polozenie_nog for i in range(4)]
+y_vals = [noga[i][1] for noga in polozenie_nog for i in range(4)]
+z_vals = [noga[i][2] for noga in polozenie_nog for i in range(4)]
+
+x_min, x_max = min(x_vals), max(x_vals)
+y_min, y_max = min(y_vals), max(y_vals)
+z_min, z_max = min(z_vals), max(z_vals)
+
+max_range = max(x_max - x_min, y_max - y_min, z_max - z_min) / 2
+
+x_mid = (x_max + x_min) / 2
+y_mid = (y_max + y_min) / 2
+z_mid = (z_max + z_min) / 2
+
+ax.set_xlim(x_mid - max_range, x_mid + max_range)
+ax.set_ylim(y_mid - max_range, y_mid + max_range)
+ax.set_zlim(z_mid - max_range, z_mid + max_range)
+
+plt.show()
+#todo znalezc punkty w ktore noga ma trafiac aby robot poruszal sie do przodu
+#todo podawac na bierzaco obrot kazdego z serw podczas chodzenia
