0d20a51f5bf200ce25ed4de25ad26fd5eb6de40d
Grzegorz Lange <59538599+Siedmiu@users.noreply.github.com>
2025-05-10 17:44:33 +0000

    Zeroing code based on @TronPL ESP
    slots 0-3 -> 0 deg for joint 0
    slots 4-7 -> 30 deg for joint 1
    slots 8-15 -> 180 deg for joint 3

---
+++ how to assemble/Hexapod Zeroing Code/src/main.cpp
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// Servo driver setup
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+const int SERVO_MIN = 102;
+const int SERVO_MAX = 512;
+
+void setServoAngle(uint8_t channel, float angle) {
+  angle = constrain(angle, 0, 180);
+  uint16_t pulse = map(angle, 0, 180, SERVO_MIN, SERVO_MAX);
+  pwm.setPWM(channel, 0, pulse);
+}
+
+void setup() {
+  Wire.begin();
+  pwm.begin();
+  pwm.setPWMFreq(60);
+
+  // Example usage
+  setServoAngle(0, 0);
+  setServoAngle(1, 0);
+  setServoAngle(2, 0);
+  setServoAngle(3, 180);
+  setServoAngle(4, 180);
+  setServoAngle(5, 180);
+}
+
+void loop() {
+  setServoAngle(0, 0);
+  setServoAngle(1, 0);
+  setServoAngle(2, 0);
+  setServoAngle(3, 0);
+
+  setServoAngle(4, 30);
+  setServoAngle(5, 30);
+  setServoAngle(6, 30);
+  setServoAngle(7, 30);
+
+  setServoAngle(8, 180);
+  setServoAngle(9, 180);
+  setServoAngle(10, 180);
+  setServoAngle(11, 180);
+
+  setServoAngle(12, 180);
+  setServoAngle(13, 180);
+  setServoAngle(14, 180);
+  setServoAngle(15, 180);
+}
