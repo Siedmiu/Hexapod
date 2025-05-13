353dc6bc2ad451769252501a388ca0f9df741e76
FlyBozon <m.flybozon@gmail.com>
2025-05-14 01:21:52 +0200

    added imu, started connections between app and real robot'

--- PlatformIO/Projects/ros2_ws/build/pajak/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/milosz/Dokumenty/Main/Hexapod/PlatformIO/Projects/ros2_ws/install/pajak'
